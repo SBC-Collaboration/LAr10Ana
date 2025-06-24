2dbebc17f05c2abda39c1542bd52253637b09692
Gray Putnam <gputnam@fnal.gov>
2025-06-24 10:31:20 -0500

    Cleanup scripts.

--- conda_init.sh
+++ conda_init.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+# Commands to create a new conda environment with the packages needed for SBC LAr10 analysis
+
+# NOTE: YOU PROBABLY DON'T NEED TO RUN THIS. USER CODE SHOULD BE RUN WITH THE CENTRAL CONDA ENVIRONMENT.
+
 conda create --name env python jupyterlab notebook numpy pandas scipy matplotlib pip nbstripout
+conda activate env
 pip install git+https://github.com/SBC-Collaboration/SBCBinaryFormat.git@master#subdirectory=python
--- jupyter_init.sh
+++ jupyter_init.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
+# Add your current kernel to jupyterlab
 python -m ipykernel install --user --name sbc-env --display-name "SBC conda (env)"
