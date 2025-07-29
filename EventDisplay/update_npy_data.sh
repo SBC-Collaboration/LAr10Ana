e00856420d3d5d899f17143905548f2887e002b7
Justin Hernandez <jmarthernandez@gmail.com>
2025-07-29 14:31:11 -0500

    Add cronjob for updating npy files and remove old files

---
+++ EventDisplay/update_npy_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script to add new event data to raw_events.npy
+set -e
+
+# Resolve absolute path of the cron script
+NPY_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# LAr10Ana project root
+PROJECT_DIR="$(dirname "$NPY_SCRIPT_DIR")"
+
+echo "Npy Script dir: $NPY_SCRIPT_DIR"
+echo "Project dir: $PROJECT_DIR"
+
+echo "Sourcing conda environment"
+source "$PROJECT_DIR/setup.sh"
+
+echo "Running convert_raw_to_npy_run_by_run.py"
+python "$NPY_SCRIPT_DIR/convert_raw_to_npy_run_by_run.py" /exp/e961/data/SBC-25-daqdata "$NPY_SCRIPT_DIR/npy/SBC-25"
+
+echo "Running merge_raw_run_npy.py"
+python "$NPY_SCRIPT_DIR/merge_raw_run_npy.py" "$NPY_SCRIPT_DIR/npy/SBC-25/"
