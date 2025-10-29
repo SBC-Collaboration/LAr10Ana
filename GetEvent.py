4d0ddd4b8cd2ee1981516450d417147b9d0fad50
Gray Putnam <gputnam@fnal.gov>
2025-10-29 16:54:18 -0500

    Create new TarStreamer class, ineherited from Streamer, which can handle .tar file inputs. Integrate into GetEvent so that we can directly process .tar file runs.

--- GetEvent.py
+++ GetEvent.py
@@ -1,9 +1,12 @@
 import os
 import numpy as np
 from sbcbinaryformat import Streamer, Writer
+from TarStreamer import TarStreamer
 from PIL import Image
 import json
 import warnings
+import tarfile
+import io
 
 full_loadlist = [
     "acoustics",
@@ -28,11 +31,44 @@ def GetScint(ev, start=None, end=None, length=None):
     return out_ev
 
 def NEvent(rundirectory):
-    return len([d for d in os.listdir(rundirectory) if os.path.isdir(os.path.join(rundirectory, d))])
+    if os.path.isdir(rundirectory):
+        return len([d for d in os.listdir(rundirectory) if os.path.isdir(os.path.join(rundirectory, d))])
+    elif rundirectory.endswith(".tar"):
+        with tarfile.open(rundirectory, "r") as tf:
+            return sum([m.isdir() for m in tf.getmembers()]) 
+    else:
+        raise ValueError("Input rundirectory (%s) must either be a directory or a tar file (.tar)" % rundirectory)
+
+def GetFiles(rundirectory, event_dir):
+    if os.path.isdir(rundirectory):
+        return os.listdir(event_dir)
+    elif rundirectory.endswith(".tar"):
+        with tarfile.open(rundirectory, "r") as tf:
+            return [m.name.split("/")[-1] for m in tf.getmembers() if m.name.startswith(event_dir + "/")]
+    else:
+        raise ValueError("Input rundirectory (%s) must either be a directory or a tar file (.tar)" % rundirectory)
+
+def FileExists(rundirectory, file_name):
+    if os.path.isdir(rundirectory):
+        return os.path.exists(file_name)
+    elif rundirectory.endswith(".tar"):
+        with tarfile.open(rundirectory, "r") as tf:
+            return file_name in tf.getnames()
+    else:
+        raise ValueError("Input rundirectory (%s) must either be a directory or a tar file (.tar)" % rundirectory)
 
 def GetEvent(rundirectory, ev, *loadlist, strictMode=True, lazy_load_scintillation=True):
     event = dict()
-    event_dir = os.path.join(rundirectory, str(ev)) 
+
+    if os.path.isdir(rundirectory):
+        is_tar = False
+    elif rundirectory.endswith(".tar"):
+        is_tar = True
+    else:
+        raise ValueError("Input rundirectory (%s) must either be a directory or a tar file (.tar)" % rundirectory)
+
+    # prepend the run directory if this isn't a tar file
+    event_dir = os.path.join(os.path.splitext(os.path.basename(rundirectory))[0], str(ev)) if is_tar else os.path.join(rundirectory, str(ev)) 
 
     for key in full_loadlist:
         event[key] = dict(loaded=False)
@@ -44,7 +80,7 @@ def GetEvent(rundirectory, ev, *loadlist, strictMode=True, lazy_load_scintillati
 
     if "acoustics" in loadlist:
         acoustic_file = None
-        for fname in os.listdir(event_dir):
+        for fname in GetFiles(rundirectory, event_dir):
             if fname.startswith("acoustics"):
                 acoustic_file = os.path.join(event_dir, fname)
                 break
@@ -55,7 +91,7 @@ def GetEvent(rundirectory, ev, *loadlist, strictMode=True, lazy_load_scintillati
             else:
                 warnings.warn("No acoustics file present in the run directory. Data will not be available in the returned dictionary.")
         else:
-            acoustic_data = Streamer(acoustic_file).to_dict()
+            acoustic_data = Streamer(acoustic_file).to_dict() if not is_tar else TarStreamer(rundirectory, acoustic_file).to_dict()
             event["acoustics"]["loaded"] = True
             for k, v in acoustic_data.items():
                 event["acoustics"][k] = v
@@ -63,19 +99,19 @@ def GetEvent(rundirectory, ev, *loadlist, strictMode=True, lazy_load_scintillati
     if "scintillation" in loadlist:
         scint_file = os.path.join(event_dir, "scintillation.sbc")
 
-        if not os.path.exists(scint_file):
+        if not FileExists(rundirectory, scint_file):
             if strictMode: 
                 raise FileNotFoundError("No scintillation file present in the run directory. To disable this error, either pass strictMode=False, or remove 'scintillation' from the loadlist")
             else:
                 warnings.warn("No scintillation file present in the run directory. Data will not be available in the returned dictionary.")
         else:
             if lazy_load_scintillation:
-                scint = Streamer(scint_file, max_size=1000)
+                scint = Streamer(scint_file, max_size=1000) if not is_tar else TarStreamer(rundirectory, scint_file, max_size=1000)
                 for c in scint.columns:
                     event["scintillation"][c] = lambda start=None, end=None, length=None: scint.to_dict(start=start, end=end, length=length)[c]
                 event["scintillation"]["length"] = scint.num_elems
             else:
-                scint = Streamer(scint_file)
+                scint = Streamer(scint_file) if not is_tar else TarStreamer(rundirectory, scint_file)
                 scint = scint.to_dict()
                 for k, v in scint.items():
                     event["scintillation"][k] = v
@@ -87,84 +123,104 @@ def GetEvent(rundirectory, ev, *loadlist, strictMode=True, lazy_load_scintillati
         for cam_ind in range(1, 4):
             event["cam"]["c%i" % cam_ind] = {}
             cam_file = os.path.join(event_dir, "cam%i-info.csv" % cam_ind)
-            if not os.path.exists(cam_file):
+            if not FileExists(rundirectory, cam_file):
                 if strictMode: 
                     raise FileNotFoundError("Missing camera file (%s) in the run directory. To disable this error, either pass strictMode=False, or remove 'cam' from the loadlist" % str(cam_file))
                 else:
                     warnings.warn("Missing camera file in the run directory. Data will not be available in the returned dictionary.")
                 continue
 
-            cam_data = np.transpose(np.loadtxt(cam_file, delimiter=",", skiprows=1))
+            if not is_tar:
+                cam_data = np.transpose(np.loadtxt(cam_file, delimiter=",", skiprows=1))
+            else:
+                with tarfile.open(rundirectory, "r") as tf:
+                    with tf.extractfile(cam_file) as f:
+                        cam_data = np.transpose(np.loadtxt(f, delimiter=",", skiprows=1))
+
             cam_data_headers = ["index"]
-            with open(cam_file) as f:
-                first_line = f.readline()
-                cam_data_headers += [s for s in first_line.rstrip("\n").split(",") if s]
+
+            if not is_tar:
+                with open(cam_file) as f:
+                    first_line = f.readline()
+                    cam_data_headers += [s for s in first_line.rstrip("\n").split(",") if s]
+            else:
+                with tarfile.open(rundirectory, "r") as tf:
+                    with tf.extractfile(cam_file) as f:
+                        first_line = f.readline().decode("utf-8")
+                        cam_data_headers += [s for s in first_line.rstrip("\n").split(",") if s]
+
             for h, d in zip(cam_data_headers, cam_data):
                 event["cam"]["c%i" % cam_ind][h] = d
 
-        for fname in os.listdir(event_dir):
+        for fname in GetFiles(rundirectory, event_dir):
             if fname.startswith("cam") and fname.endswith(".png"):
                 img_file = os.path.join(event_dir, fname)
                 cam_ind = int(fname[3])
                 frame_ind = int(fname[8:10])
-                event["cam"]["c%i" % cam_ind]["frame%i" % frame_ind] = np.array(Image.open(img_file).convert("RGB"))
+
+                if not is_tar:
+                    event["cam"]["c%i" % cam_ind]["frame%i" % frame_ind] = np.array(Image.open(img_file).convert("RGB"))
+                else:
+                    with tarfile.open(rundirectory, "r") as tf:
+                        with tf.extractfile(img_file) as f:
+                            event["cam"]["c%i" % cam_ind]["frame%i" % frame_ind] = np.array(Image.open(io.BytesIO(f.read())).convert("RGB"))
 
     if "event_info" in loadlist:
         event_file = os.path.join(event_dir, "event_info.sbc")
 
-        if not os.path.exists(event_file):
+        if not FileExists(rundirectory, event_file):
             if strictMode: 
                 raise FileNotFoundError("No event_info file present in the run directory. To disable this error, either pass strictMode=False, or remove 'event_info' from the loadlist")
             else:
                 warnings.warn("No event_info file present in the run directory. Data will not be available in the returned dictionary.")
         else:
-            event_data = Streamer(event_file).to_dict()
+            event_data = Streamer(event_file).to_dict() if not is_tar else TarStreamer(rundirectory, event_file).to_dict()
             event["event_info"]["loaded"] = True
             for k, v in event_data.items():
                 event["event_info"][k] = v
 
     if "slow_daq" in loadlist:
         slow_daq_file = os.path.join(event_dir, "slow_daq.sbc")
-        if not os.path.exists(slow_daq_file):
+        if not FileExists(rundirectory, slow_daq_file):
             if strictMode: 
                 raise FileNotFoundError("No slow_daq file present in the run directory. To disable this error, either pass strictMode=False, or remove 'slow_daq' from the loadlist")
             else:
                 warnings.warn("No slow_daq file present in the run directory. Data will not be available in the returned dictionary.")
         else:
-            slow_daq_data = Streamer(slow_daq_file).to_dict()
+            slow_daq_data = Streamer(slow_daq_file).to_dict() if not is_tar else TarStreamer(rundirectory, slow_daq_file).to_dict()
             event["slow_daq"]["loaded"] = True
             for k, v in slow_daq_data.items():
                  event["slow_daq"][k] = v
 
     if "plc" in loadlist:
         plc_file = os.path.join(event_dir, "plc.sbc")
-        if not os.path.exists(plc_file):
+        if not FileExists(rundirectory, plc_file):
             if strictMode: 
                 raise FileNotFoundError("No plc file present in the run directory. To disable this error, either pass strictMode=False, or remove 'plc' from the loadlist")
             else:
                 warnings.warn("No plc file present in the run directory. Data will not be available in the returned dictionary.")
         else:
-            plc_data = Streamer(plc_file).to_dict()
+            plc_data = Streamer(plc_file).to_dict() if not is_tar else TarStreamer(rundirectory, plc_file).to_dict()
             event["plc"]["loaded"] = True
             for k, v in plc_data.items():
                 event["plc"][k] = v
 
     if "run_info" in loadlist:
         run_info_file = os.path.join(rundirectory, "run_info.sbc")
-        if not os.path.exists(run_info_file):
+        if not FileExists(rundirectory, run_info_file):
             if strictMode: 
                 raise FileNotFoundError("No run_info file present in the run directory. To disable this error, either pass strictMode=False, or remove 'run_info' from the loadlist")
             else:
                 warnings.warn("No run_info file present in the run directory. Data will not be available in the returned dictionary.")
         else:
-            run_info_data = Streamer(run_info_file).to_dict()
+            run_info_data = Streamer(run_info_file).to_dict() if not is_tar else TarStreamer(rundirectory, run_info_file).to_dict()
             event["run_info"]["loaded"] = True
             for k, v in run_info_data.items():
                 event["run_info"][k] = v
 
     if "run_control" in loadlist:
         run_ctrl_file = os.path.join(rundirectory, "rc.json")
-        if not os.path.exists(run_ctrl_file):
+        if not FileExists(rundirectory, run_ctrl_file):
             if strictMode: 
                 raise FileNotFoundError("No run_control file present in the run directory. To disable this error, either pass strictMode=False, or remove 'run_control' from the loadlist")
             else:
