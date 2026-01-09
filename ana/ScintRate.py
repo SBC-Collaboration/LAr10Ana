c171e3343ea5d9a8313d3b5a4a67e78ebb6f4567
Zhiheng Sheng <25388025+shengzhiheng@users.noreply.github.com>
2026-01-08 20:51:32 -0600

    Gary's updated scint_rate module, removing FFT with new method, which hopefully fixes memory leak

--- ana/ScintRate.py
+++ ana/ScintRate.py
@@ -6,46 +6,19 @@ import numpy as np
 
 # Functions to run code
 
-# FFT cuts based on FFT shape
-# Good waveforms should have max FFT near 0 MHz
-def _FFT_frequency_filtering(waveforms, dt, freq_cutoff_hz):
-    # Number of samples in waveform
-    N = waveforms.shape[-1]
-
-    # Compute the FFT values and frequencies
-    fft_vals = np.abs(np.fft.fft(waveforms, axis=-1))
-    fft_freq = np.abs(np.fft.fftfreq(N, d=dt))
-
-    # Find where the maximum FFT val occurs
-    max_amp_indices = np.argmax(fft_vals[:, :, 1:], axis=-1)
-
-    # Don't need the fft_vals anymore
-    del fft_vals
-
-    # Get the frequency a which the max FFT val occurs
-    peak_frequencies_hz = fft_freq[max_amp_indices]
-
-    # Create a mask for noisy waveforms
-    bad_channel_mask = peak_frequencies_hz >= freq_cutoff_hz
-
-    # If it's a noisy waveform, replace it with zeros.
-    filtered_waveforms = waveforms.copy()
-    filtered_waveforms[bad_channel_mask] = 0.
-    return filtered_waveforms
-
-
-# Computing the signal strength of waveforms
-def _calculate_signal_strength(fft_filtered_waveforms):
+# Calculate the ratio of the SiPM pulse to the baseline
+def _signal_ratio_filtering(waveforms):
 
     # Compute the baseline of each waveform
-    baseline = np.mean(fft_filtered_waveforms, axis=2)
+    baseline = np.mean(waveforms[:,:,:50], axis=-1)
+    waveforms = waveforms - baseline[:, :, np.newaxis]
 
-    # Calculate signal strength
-    signalPeak = np.min(fft_filtered_waveforms, axis=2) - baseline
-    signalDroop = np.max(fft_filtered_waveforms, axis=2) - baseline
-    signalStrength = signalDroop + signalPeak
+    # Calculate the ratio of the signal, noise should be close to 1 (cancels out)
+    signalDown = np.min(waveforms, axis=-1)
+    signalUp = np.max(waveforms, axis=-1)
+    signalRatio = np.abs(signalDown/signalUp)
 
-    return signalStrength
+    return signalRatio
 
 # Simple function to unwrap CAEN timestamps
 def _unwrap_caen_timestamp(ts, max_ts):
@@ -81,19 +54,12 @@ def ScintillationRateAnalysis(ev):
 
     if decimation == 0: decimation = 1
     dt = 1 / (sample_rate * decimation)        
-
-    # Denoise waveforms and collect signal strength
-    fft_filtered_waveforms = _FFT_frequency_filtering(
-        waveforms=waveforms, 
-        dt=dt, 
-        freq_cutoff_hz = 0.6 # MHz (Still rough guess, works well)
-    )
     
-    signal_strength = _calculate_signal_strength(fft_filtered_waveforms)
+    signal_ratio = _signal_ratio_filtering(waveforms)
 
-    # Find all signals with strength < -10 (should be good pulses).
-    signal_strength_limit = -10.
-    mask = signal_strength < signal_strength_limit
+    # Find all signals with a ratio of peak to baseline > 3, these should be good pulses.
+    signal_ratio_limit = 3.
+    mask = signal_ratio >= signal_ratio_limit
 
     # Sum the total number of hits in the event
     NHits = np.sum(mask, axis=1).astype(np.uint8)
