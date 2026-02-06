a516d2dd0c47bffe6de1b9fc1478cd4bc6551f3f
Justin Hernandez <jmarthernandez@gmail.com>
2026-02-06 00:30:38 -0600

    Fix cronjob

--- EventDisplay/update_npy_data.sh
+++ EventDisplay/update_npy_data.sh
@@ -16,6 +16,7 @@ echo "Sourcing conda environment"
 source "$PROJECT_DIR/setup.sh"
 
 echo "Running convert_raw_to_npy_run_by_run.py"
+cd "$NPY_SCRIPT_DIR"
 python "$NPY_SCRIPT_DIR/convert_raw_to_npy_run_by_run.py" /exp/e961/data/SBC-25-daqdata "$NPY_SCRIPT_DIR/npy/SBC-25"
 
 echo "Running merge_raw_run_npy.py"
