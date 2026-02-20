15e6a01ebcba3befe7c6dd127e914fff54cfce65
alkucich <130165670+alkucich@users.noreply.github.com>
2026-02-19 21:54:55 -0600

    Usable bubble finder for single bubble detection
    Multi-bubble functionality not read yet; for now, it might pick out very visible additional bubbles with radius close to that of the primary bubble.  Very consistent for single bubble events; finds t0 to ~20ns or 2 frames in at least one camera.  Still not that fast; processes one event in ~10s.  Improvements to radius approximation, but still issues with correctly getting bubble radii.  Takes an event as input, returns dictionary containing center coordinates (reliable), radius (currently unreliable), significance (ratio of highest number of votes in the accumulator array to the number of votes associated with the bubble; should be 1 for all primary bubbles, <1 for additional bubbles in a future version that better supports those events), frame number, and camera number.  Can use frame number to get t0.

---
+++ ana/BF_20260219.py
@@ -0,0 +1,234 @@
+from sbcbinaryformat import Streamer, Writer
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.draw import circle_perimeter, disk
+from skimage.measure import label, regionprops
+import diplib as dip
+
+#ev: event
+
+#cam: camera
+
+#num_pix_in_neighborhood: radius around the center of an identified bubble that 
+#is zeroed out for all radii in the accumulator array before looking at the next bubble candidate;
+#prevents the same bubble from being tagged twice 
+
+#noise_thresh: diff values below this threshold will be set to zero
+
+def FindBubbles(ev, cam, num_pix_in_neighborhood, noise_thresh, start_num):
+    #get mask for bubble region based on camera
+    refIm = np.float32(np.average(ev['cam'][f'c{cam}']['frame0'],axis=2))
+    imShape = refIm.shape
+    Tshape = imShape[::-1]
+    
+    if cam==1:
+        circy, circx = disk((375, 590), 280, shape=imShape)
+        coord_mask = 2.2*circx-1375<circy
+        mask_circx = circx[coord_mask]
+        mask_circy = circy[coord_mask]
+    elif cam==2:
+        mask_circy, mask_circx = disk((425, 670), 270, shape=imShape)
+    elif cam==3:
+        mask_circy, mask_circx = disk((440, 690), 270, shape=imShape)
+
+    #initialize some variables and the bubble dictionary to be returned at the end
+    bub_dict = {}
+    bub_num = start_num
+    bubs_found = False
+    first_found = False
+    stop_frame = 100
+
+    #get number of frames in event
+    keys = ev['cam'][f'c{cam}'].keys()
+    frames = 0
+    for key in keys:
+        if 'frame' in key:
+            frames+=1
+
+    for i in range(1,frames-1):
+
+        if i>stop_frame:
+            break
+    
+        im_num = i
+    
+        thisIm = np.float32(np.average(ev['cam'][f'c{cam}'][f'frame{im_num-1}'],axis=2))
+        nextIm = np.float32(np.average(ev['cam'][f'c{cam}'][f'frame{im_num}'],axis=2)) #may have to be changed later to account for missing frames
+    
+        #get diff and mask out noise
+        preMask_diff = abs(thisIm-nextIm)
+        preMask_diff[preMask_diff<noise_thresh] = 0
+        
+        #apply mask to isolate bubble region depending on which camera we're viewing from
+        diff = np.zeros((imShape[0],imShape[1]))
+        diff[mask_circy,mask_circx] = preMask_diff[mask_circy,mask_circx]
+        diff-=dip.GetSinglePixels(diff > 0)
+    
+        #find and label connected regions of nonzero pixels 
+        #connectivity = 2 allows pixels to count as connected if they are diagonal from each other
+        labeled = label(diff>0, connectivity = 2)
+    
+        #get properties of labeled regions
+        props = regionprops(labeled, intensity_image = diff)
+    
+        #get 2.5 sigma threshold for region intensity
+        intensities = [prop.intensity_mean for prop in props]
+        intensity_thresh = np.average(intensities) + 2.5*np.std(intensities)
+    
+        #get areas so we can identify number of potential bubbles in image
+        areas = np.array([prop.area for prop in props])
+        area_mask = areas>=areas.max()/4
+        largest_regions = np.array(props)[area_mask]
+    
+        #get region with most connected pixels 
+        largest_region = max(largest_regions, key=lambda prop: prop.area)
+    
+        #see if largest region meets area and intensity thresholds
+        #if yes, or if we saw a bubble in a previous frame, we have a bubble
+        if (largest_region.area>=10 and largest_region.intensity_mean>=intensity_thresh) or bubs_found==True:
+    
+            if bubs_found==False:
+                stop_frame = im_num+5
+                bubs_found = True
+                first_found = True
+                
+            #estimate radii
+            min_est_rad = np.round((largest_region.axis_minor_length+largest_region.axis_major_length)/4)
+            if min_est_rad - 3 <=3:
+                min_rad = 3
+                max_rad = 6
+            else:
+                min_rad = min_est_rad - 2
+                max_rad = min_est_rad + 3
+            rad_cands = np.arange(min_rad,max_rad,1)
+            
+            #perform CHT
+            for rad in rad_cands:
+                circx, circy = circle_perimeter(600, 400, int(rad), shape=Tshape)
+                dx = circx-600
+                dy = circy-400
+                offsets = [(dx[i],dy[i]) for i in range(len(dx))]
+    
+                this_layer = np.zeros((imShape[0],imShape[1]))
+                for offset in offsets:
+                    this_layer += np.roll(diff,offset,(0,1))
+    
+                if rad==rad_cands[0]:
+                    accum = this_layer
+                else:
+                    accum = np.dstack((accum,this_layer))
+    
+            accum_shape = accum.shape
+    
+            #get vote threshold -- 80% of highest peak number of votes
+            largest_cy, largest_cx = largest_region.centroid
+            largest_y, largest_x = disk((largest_cy,largest_cx), 20, shape=imShape)
+            max_votes = np.max(accum[largest_y,largest_x])
+            vote_thresh = max_votes*.8
+            
+            #now we enter the candidate loop and decide based on votes whether to keep or discard each candidate
+            for region in largest_regions:
+           
+                #helpful check for frames with no bubbles
+                if region.axis_minor_length== 0 or region.axis_major_length==0 or region.area<10:
+                    continue
+                
+                #if this is the first frame in which we see a bubble, scan back some frames to get true t0
+                if first_found==True:
+                    
+                    #look for bubble in relevant region in past images
+                    t0_found = False
+                    j = 1
+                    while t0_found==False:
+                        
+                        #get diff
+                        pastIm = np.float32(np.average(ev['cam'][f'c{cam}'][f'frame{im_num-j}'],axis=2))
+                        pastDiff = abs(pastIm - refIm)
+                        pastDiff[pastDiff<noise_thresh] = 0
+                        pastDiff-=dip.GetSinglePixels(pastDiff > 0)
+    
+                        #perform CHT
+                        rad_cands = [2,3,4]
+                        for rad in rad_cands:
+                            circx, circy = circle_perimeter(600, 400, int(rad), shape=Tshape)
+                            dx = circx-600
+                            dy = circy-400
+                            offsets = [(dx[i],dy[i]) for i in range(len(dx))]
+    
+                            this_layer = np.zeros((imShape[0],imShape[1]))
+                            for offset in offsets:
+                                this_layer += np.roll(pastDiff,offset,(0,1))
+    
+                            if rad==rad_cands[0]:
+                                past_accum = this_layer
+                            else:
+                                past_accum = np.dstack((accum,this_layer))
+    
+                        past_accum_shape = past_accum.shape
+    
+                        #paste relevant region of accumulator onto blank image
+                        pastregIm = np.zeros(past_accum_shape)
+                        pastregIm[largest_y,largest_x] = past_accum[largest_y,largest_x]
+    
+                        #find peak candidate 
+                        pcy, pcx, rad_ind = np.unravel_index(np.argmax(pastregIm), past_accum_shape)
+                        prad = rad_ind + rad_cands[0]
+    
+                        #check if bubble candidate meets intensity thresh for the noise in the image
+                        past_intensity_thresh = np.average(pastDiff) + 2.5*np.std(pastDiff)
+                        bub_coords_y, bub_coords_x = disk((pcy,pcx), prad, shape=imShape)
+                        if np.average(pastDiff[bub_coords_y,bub_coords_x]) >= past_intensity_thresh:
+                            #add bubble to dictionary
+                            this_bub_dict = {}
+                            this_bub_dict['Pos'] = (pcx,pcy)
+                            this_bub_dict['Radius'] = prad
+                            this_bub_dict['Significance'] = 1.0
+                            this_bub_dict['Frame'] = im_num-j
+                            this_bub_dict['Camera'] = cam
+                            bub_dict[f'Bubble{bub_num}'] = this_bub_dict
+                            bub_num+=1
+                            j+=1
+                    
+                        else:
+                            #if no bubble, don't bother scanning through remaining frames
+                            t0_found = True
+                    
+                    first_found = False
+                
+                #now continue on finding bubbles in successive frames as usual
+                #get number of votes in this region of the accumulator
+                rcy, rcx = region.centroid
+                ry, rx = disk((rcy,rcx), 20, shape=imShape)
+                regIm = np.zeros(accum_shape)
+                regIm[ry,rx] = accum[ry,rx]
+                votes = np.max(regIm)
+                
+                if votes>=vote_thresh:
+                    
+                    #get peak candidate in this constrained region of connected pixels
+                    pcy, pcx, rad_ind = np.unravel_index(np.argmax(regIm), accum_shape)
+                    prad = rad_ind + min_rad
+    
+                    #add bubble to dictionary
+                    this_bub_dict = {}
+                    this_bub_dict['Pos'] = (pcx,pcy)
+                    this_bub_dict['Radius'] = prad
+                    this_bub_dict['Significance'] = votes/max_votes
+                    this_bub_dict['Frame'] = im_num
+                    this_bub_dict['Camera'] = cam
+                    bub_dict[f'Bubble{bub_num}'] = this_bub_dict
+                    bub_num+=1
+                
+                    #zero out accumulator array for all radii around this point
+                    circy, circx = disk((pcy, pcx), prad+num_pix_in_neighborhood, shape=imShape)
+                    accum[circy,circx] = 0
+
+    return bub_dict
+
+def GetBubDict(ev, num_pix_in_neighborhood = 20, noise_thresh = 10):
+        
+    cam_dict1 = FindBubbles(ev, 1, num_pix_in_neighborhood, noise_thresh, start_num = 0)
+    cam_dict2 = FindBubbles(ev, 2, num_pix_in_neighborhood, noise_thresh, start_num = len(cam_dict1))
+    cam_dict3 = FindBubbles(ev, 3, num_pix_in_neighborhood, noise_thresh, start_num = len(cam_dict1)+len(cam_dict2))
+
+    return cam_dict1 | cam_dict2 | cam_dict3
