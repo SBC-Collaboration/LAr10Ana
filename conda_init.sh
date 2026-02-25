71c68ec351510d78c16bb99825815d30eeb28d72
Gray Putnam <gputnam@fnal.gov>
2026-02-25 08:55:33 -0600

    Add additional dependencies for bubble finding.

--- conda_init.sh
+++ conda_init.sh
@@ -4,6 +4,6 @@
 
 # NOTE: YOU PROBABLY DON'T NEED TO RUN THIS. USER CODE SHOULD BE RUN WITH THE CENTRAL CONDA ENVIRONMENT.
 
-conda create --name env python jupyterlab notebook numpy pandas scipy matplotlib pip nbstripout opencv pymysql spyder plotly ipympl tqdm multiprocess pywavelets
+conda create --name env python jupyterlab notebook numpy pandas scipy matplotlib pip nbstripout opencv pymysql spyder plotly ipympl tqdm multiprocess pywavelets scikit-image diplib
 conda activate env
 pip install git+https://github.com/SBC-Collaboration/SBCBinaryFormat.git@master#subdirectory=python
