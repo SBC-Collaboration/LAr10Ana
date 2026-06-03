28c6ab71d2936fc8ff43b7c2b77ecac9d832f02f
Olivia Chiarini <chiariniolivia@gmail.com>
2026-06-03 15:34:06 -0400

    changed output to be set of coordinates

--- ana/Reconstruction3D.py
+++ ana/Reconstruction3D.py
@@ -3,8 +3,6 @@ from collections import Counter, defaultdict
 import numpy as np
 
 
-
-
 '''
 Least squares triangulation, 2D to 3D points. Needs 2+ cams
 '''
@@ -24,15 +22,9 @@ def triangulate_multi_cam_LS(pixel_coords):
     SiPMs and fiducial markings as training points
     '''
 
-    P1 = np.array([[-1.05302109e+02, -7.02444185e+02, -3.34577970e+02,  5.72535995e+03],
-     [-5.51213766e+02,  2.58404210e+01, -3.45420423e+02,  3.46877200e+03],
-     [ 5.46200003e-02, -3.31725499e-01, -9.41793422e-01,  8.93247437e+00]])
-    P2 = np.array([[ 6.24551374e+02,  2.05426176e+02, -4.20327029e+02,  6.08648299e+03],
-     [ 2.38142395e+02, -5.16479247e+02, -3.98154885e+02,  3.57897785e+03],
-     [ 1.75014306e-01,  8.21425255e-02, -9.81133323e-01,  8.45879059e+00]])
-    P3 = np.array([[-4.46470566e+02,  4.77173422e+02, -4.42541834e+02,  5.80637791e+03],
-     [ 3.67166284e+02,  4.75216795e+02, -4.43358757e+02,  3.38952193e+03],
-     [-9.35610736e-02,  1.48157021e-01, -9.84528223e-01,  7.62754209e+00]])
+    P1 = getProjMat(1)
+    P2 = getProjMat(2)
+    P3 = getProjMat(3)
 
     P_mats = [P1, P2, P3]
 
@@ -53,7 +45,7 @@ def triangulate_multi_cam_LS(pixel_coords):
         A.append(y * P[2] - P[1])
 
     if valid_cam_count < 2:
-        return np.array([np.nan, np.nan, np.nan])
+        return np.array([-1,-1, -1])
 
     A = np.array(A)
 
@@ -63,6 +55,28 @@ def triangulate_multi_cam_LS(pixel_coords):
 
     return X[:3]
 
+
+
+def getProjMat(camNum):
+    if camNum == 1:
+        return np.array([[-1.05302109e+02, -7.02444185e+02, -3.34577970e+02,  5.72535995e+03],
+                [-5.51213766e+02,  2.58404210e+01, -3.45420423e+02,  3.46877200e+03],
+                [ 5.46200003e-02, -3.31725499e-01, -9.41793422e-01,  8.93247437e+00]])
+
+
+    if camNum == 2:
+        return np.array([[ 6.24551374e+02,  2.05426176e+02, -4.20327029e+02,  6.08648299e+03],
+                [ 2.38142395e+02, -5.16479247e+02, -3.98154885e+02,  3.57897785e+03],
+                [ 1.75014306e-01,  8.21425255e-02, -9.81133323e-01,  8.45879059e+00]])
+
+    
+    if camNum == 3:
+        return np.array([[-4.46470566e+02,  4.77173422e+02, -4.42541834e+02,  5.80637791e+03],
+                [ 3.67166284e+02,  4.75216795e+02, -4.43358757e+02,  3.38952193e+03],
+                [-9.35610736e-02,  1.48157021e-01, -9.84528223e-01,  7.62754209e+00]])
+    return np.array([[np.nan,  np.nan, np.nan,  np.nan],
+     [np.nan, np.nan, np.nan, np.nan],
+     [np.nan,np.nan,np.nan,np.nan]])
 
 
 '''
@@ -190,11 +204,28 @@ def pull_bubble_coords(bubble_data):
 
 
 def reconstruct_2D_to_3D(data):
+
+    """
+    OUTPUT KEY:
+    -999, -999, -999: Only a single cam is defined
+    -1000,-1000,-1000: Possible multi bubble event
+    NaN, NaN, NaN: No bubble finder info for this frame/event
+    """
+
+    """
+    bubble mult check:
+        if failed, return list of len 49 of lists of 3 -999 values
+    """
+
     if "bubble" in data["analysis"]:
         bubble_data = data["analysis"]["bubble"]
         
         if bubble_mult(bubble_data) != 1:
-            return {"coords_3D": np.full(3, np.nan)}
+            coordsToReturn = []
+            for i in range(50):
+                coordsToReturn.append(np.full(3, -999)
+
+            return {"coords_3D": np.full(3, -2)}
 
         # Pull 2D coordinate
         coords_2D = pull_bubble_coords(bubble_data)
@@ -205,7 +236,10 @@ def reconstruct_2D_to_3D(data):
         return {"coords_3D": coords_3D}
 
     else:
-        return {"coords_3D": np.full(3, np.nan)}
+        coordsToReturn = []
+        for i in range(50):
+            coordsToReturn.append(np.full(3,np.nan)
+        return {"coords_3D": coordsToReturn}
 
 
 
